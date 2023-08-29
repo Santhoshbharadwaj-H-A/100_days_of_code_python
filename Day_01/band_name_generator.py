5835f24048ccccf747c7be8ec6e72a5036f24ef6
hasanthoshbharadwaj <hasanthoshbharadwaj@gmail.com>
2023-08-29 16:19:18 +0530

    band_name_generator

--- Day_01/band_name_generator.py
+++ Day_01/band_name_generator.py
@@ -1,3 +1,11 @@
 # 100 days of code
-#greeting th user
+# greeting th user
 print("welcome to the band name generator")  
+
+# taking inputs
+city = input("whats the name of the ciyt you grew up? \n")
+pet = input("what is the name of the pet \n")
+
+#combine the name of the city and pet and show the results
+print("your band name cloud be" + city + " " + pet +" ")
+
